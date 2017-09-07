b9ea788bcb8419e6f5ab5b332a41864e1331b515
jianmyphy123 <jianmyphy123@tutanota.com>
2017-09-07 17:16:40 +0200

    init

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'src/build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './src/build', 'index.html'));
+});
+
+app.listen(PORT, process.env.IP, function(){
+  console.log("Server has started " + PORT);
+});
